8b688ed97605b1d5cc65925480d10cd9d295ed2e
Generosa Litton <glcodeworks@gmail.com>
2024-05-25 16:23:07 -0500

    final round still not working

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -13,14 +13,14 @@ const WINNING_COMBOS = {
 let playerScore = 0;
 let computerScore = 0;
 let tie = 0;
+let round = 1;
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-let round = 1;
 function displayRound() {
-  prompt(`${MESSAGES["round"]}${round} of ${TOTAL_ROUNDS}`);
+  prompt(`------ ${MESSAGES["round"]}${round} of ${TOTAL_ROUNDS} ------ `);
 }
 
 function playerWins(choice, computerChoice) {
@@ -47,7 +47,7 @@ function displayRoundWinner(choice, computerChoice) {
     computerScore += 1;
   } else {
     displayTie(choice, computerChoice);
-    tie += 1; // check this, why is it saying it isn't declared???
+    tie += 1;
     prompt(MESSAGES["tie"]);
   }
 }
@@ -57,10 +57,9 @@ function displayRoundScores(playerScore, computerScore) {
     `Your score is ${playerScore} while the computer's score is ${computerScore}`
   );
 }
-// New array with shortened version of the valid choices
+
 let shortenedChoices = VALID_CHOICES.map((item) => item.slice(0, 2));
 
-//function to convert shortened choices back to real words
 let validUserChoice;
 function finalUserChoice(choice) {
   switch (choice) {
@@ -88,17 +87,40 @@ function showGameWinner(playerScore, computerScore) {
     prompt("you win the game!");
   } else if (computerScore === 3 && round <= 5) {
     prompt("computer wins the game!");
-  } else if (playerScore === computerScore && round < 5) {
-    prompt("No winner yet, on to the next round.");
   } else if (playerScore === computerScore && round > 5) {
     prompt("Game over, no one won three out of five!");
   }
 }
 
+// function playFinalRound(round, playerScore, computerScore) {
+//   while (round !== 6) {
+//     showGameWinner(playerScore, computerScore);
+//     prompt(MESSAGES["anotherGame"]);
+//     let playAgainAnswer = readline.question();
+//     console.clear();
+
+//     while (!["y", "n"].includes(playAgainAnswer)) {
+//       prompt(MESSAGES["invalidChoice"]);
+//       playAgainAnswer = readline.question();
+//       console.clear();
+//     }
+//     if (playAgainAnswer !== "y") {
+//       console.clear();
+//       break;
+//     }
+//   }
+// }
+
+prompt(MESSAGES["welcome"]);
+prompt(`${MESSAGES["winner"]}`);
+
+// GAME STARTS HERE
 while (round <= TOTAL_ROUNDS) {
-  prompt(MESSAGES["welcome"]);
-  prompt(`${MESSAGES["winner"]}`);
   displayRound();
+  if (round > 1) {
+    displayRoundScores(playerScore, computerScore);
+  }
+
   round += 1; // increment until 5
   prompt(
     `${MESSAGES["description1"]} ${shortenedChoices.join(", ")}\n ${
@@ -140,6 +162,8 @@ while (round <= TOTAL_ROUNDS) {
       break;
     }
   }
+
+  // playFinalRound(round, playerScore, computerScore);
 
   prompt(MESSAGES["nextRound"]);
   let nextRoundAnswer = readline.question();
