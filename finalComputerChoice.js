de7a464f013b7cc1cd12ffc2394efcebd4833b41
Generosa Litton <glcodeworks@gmail.com>
2024-05-29 11:42:47 -0500

    testing file

--- finalComputerChoice.js
+++ finalComputerChoice.js
@@ -1,16 +1,5 @@
-// const readline = require("readline-sync");
-// const MESSAGES = require("./game_messages.json");
-
-// const TOTAL_ROUNDS = 5;
-// const WINNING_SCORE = 3;
+// File for testing function
 const VALID_CHOICES = ["rock", "paper", "scissors", "lizard", "spock"];
-// const WINNING_COMBOS = {
-//   rock: ["scissors", "lizard"],
-//   paper: ["rock", "spock"],
-//   scissors: ["paper", "lizard"],
-//   lizard: ["paper", "spock"],
-//   spock: ["rock", "scissors"],
-// };
 
 function finalComputerChoice() {
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
